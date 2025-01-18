907e3cceb5c33237977e8996b845291392cea752
Rupendra Kumar Dhiman <Dhiman.RupendraK@hutchisonports.com>
2025-01-19 00:28:09 +0800

    Initial Commit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
+import logo from '../../logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+export const  App = ()=> {
+  return <div className="App">
+    <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -19,8 +17,5 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
+    </div>;
 }
-
-export default App;
