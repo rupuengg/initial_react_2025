a83dd7a356fcd1f39649e2eae3fc52484627b0f3
Rupendra Kumar Dhiman <Dhiman.RupendraK@hutchisonports.com>
2025-01-19 16:30:15 +0800

    Prettier installatio

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,4 +14,23 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "sort-imports": [
+        "error", {
+          "ignoreCase": false,
+          "ignoreDeclarationSort": true,
+          "ignoreMemberSort": false,
+          "memberSyntaxSortOrder": ["none", "all", "multiple", "single"],
+          "allowSeparatedGroups": false
+        }
+      ],
+      "react/prop-types": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+    }
+  }
 ];
