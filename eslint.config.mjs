4659049862144eae080141cac84dfe187fe2e857
Rupendra Kumar Dhiman <Dhiman.RupendraK@hutchisonports.com>
2025-01-19 20:13:50 +0800

    Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -14,6 +15,7 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  eslintPluginPrettierRecommended,
   {
     rules: {
       "react/react-in-jsx-scope": "off",
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { reportWebVitals } from './reportWebVitals';
-import { App } from 'components';
-import './index.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<React.StrictMode><App /></React.StrictMode>);
-
-reportWebVitals();
+import React from 'react';import ReactDOM from 'react-dom/client';import { reportWebVitals } from './reportWebVitals';import { App } from 'components';import './index.css';const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);root.render(  <React.StrictMode>    <App />  </React.StrictMode>);reportWebVitals();
